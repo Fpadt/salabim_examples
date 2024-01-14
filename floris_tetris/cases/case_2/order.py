b1fa85ae864242d675a09f7952329a0be95f1307
floris Padt <floris@fmpadt.nl>
2024-01-14 17:02:06 +0100

    All priority rules added and tested

--- floris_tetris/cases/case_2/order.py
+++ floris_tetris/cases/case_2/order.py
@@ -9,9 +9,10 @@ class EV(Component):
         self.dur = dur  #    duration of stay
         self.toa = None  #   time of arrival
         self.tod = None  #   time of departure (estimated)
-        self.tcb = None  #   time of charge begin
         self.dsc = dsc  #    desired state of charge in kWh
         self.csc = 0  #      current state f charge
+        self.t2c = dsc/mpi # estimated time to charge
+        self.llx = None  #   least Laxity
         # --- Monitors ---
         self.mon_kwh = Monitor(name="EV kWh", level=True)
         self.mon_dur = Monitor(name="EV Dur", level=False)
@@ -35,10 +36,10 @@ class EVSE(Component):
         # --- Constants ---
         self.mpo = mpo  #    max power output
         # --- Variables ---
-        # self.tcs = None  #   time of charge start
-        # self.pcs = None  #   power at charge start
+        # self.tcs = None  # time of charge start
+        # self.pcs = None  # power at charge start
         self.pwr = 0  #      charging power
-        # self.kwh = 0  #      kWh delivered
+        # self.kwh = 0  #    kWh delivered
         # --- Objects ---
         self.ev = None  #    EV being charged
         # --- Monitors ---
@@ -55,6 +56,9 @@ class EVSE(Component):
                 self.passivate()
             # assign EV
             self.ev = waitingline.pop()
+            self.ev.toa = env.now()
+            self.ev.tod = self.ev.toa + self.ev.dur
+            self.ev.llx = self.ev.tod - self.ev.t2c
             # charge EV
             self.hold(self.ev.dur)
 
@@ -69,22 +73,28 @@ class EVSE(Component):
 
 class TGC(Component):
     def setup(self):
+        # --- Objects ---
         self.schedule = env.Queue("SCHEDULE")
 
     def make_schedule(self, property_name):
+        """Make a schedule of EVSEs sorted on property_name"""
         # new schedule
         self.schedule.clear()
 
-        # sort and reverse sort
+        # If property_name starts with a minus sign, reverse the order
         reverse = 1
         if property_name[0] == "-":
             reverse = -1
             property_name = property_name[1:]
 
+        # Create schedule sorted on property_name
+        # add the EVSE without EV at then end to make it complete
         for evse in HUB:
             if evse.ev is not None:
                 property_value = reverse * getattr(evse.ev, property_name)
-                self.schedule.add_sorted(evse, property_value)
+            else:
+                property_value = float("inf")
+            self.schedule.add_sorted(evse, property_value)
 
         return self.schedule
 
@@ -100,32 +110,43 @@ class TGC(Component):
             )
 
     def assign_power(self):
-        # self.request((enx,0))
-        # self.release(enx)
-        remaining_power = ENX_MPO
-        order = self.make_schedule(PRIO)
+        """Assign power to EVSEs according to priority
+        Start with the total Power available fro Enexis
+        assign according to priority set by sorting on property
+        """
+        # initialize the available power to the total power available
+        available_power = ENX_MPO
+        # make a schedule
+        order = self.make_schedule(RLS[RUL])
         # order.print_info()
+        # assign power to EVSEs
         while len(order) > 0:
             evse = order.pop()
-            evse.ev.csc += evse.pwr * (
-                env.now() - evse.ev.tcb
-            )  # store charge, move to method of EV
-            evse.ev.tcb = env.now()  # move to mthod of EV
-            evse.pwr = min([evse.mpo, evse.ev.mpi, remaining_power])
+            if evse.ev == None:
+                evse.pwr = 0
+            else:
+                evse.pwr = min(
+                    [
+                        evse.mpo,
+                        available_power,
+                        evse.ev.mpi,
+                    ]
+                )
 
-            remaining_power -= evse.pwr  # note may not be negative
-            remaining_power = max(remaining_power, 0)  # note may not be negative
+            available_power -= evse.pwr
+            available_power = max(available_power, 0)  # note may not be negative
+            print(f"available_power: {available_power}")
 
     def update_se_enerygy_charged(self):
         for evse in HUB:
             if evse.ev is not None:
                 # note first update power next energy
-                evse.pwr = min([evse.mpo, evse.ev.mpi])
+                # evse.pwr = min([evse.mpo, evse.ev.mpi])
                 evse.update_energy_charged()
 
     def process(self):
         while True:
-            # self.assign_power() TODO
+            self.assign_power()  # TODO
             self.update_se_enerygy_charged()
             # statistics
             [self.print_state(x) for x in HUB]
@@ -138,15 +159,25 @@ class TGC(Component):
 # --------------------------------------------------------------------------
 env = Environment(trace=False, random_seed=42)
 
-ENX_MPO = 3 * 7
-PRIO = "tod"  # FIFO = toa, LIFO = -toa, EDD = tod, LL = <to_e_developed>
+RLS = {
+    "EDD": "tod",  #         tested and Ok
+    "LDD": "-tod",  #        tested and Ok
+    "FIFO": "toa",  #        tested and Ok
+    "LIFO": "-toa",  #       tested and Ok
+    "SPT": "t2c",  #         tested and Ok
+    "LPT": "-t2c",  #        tested and Ok        
+    "LLX": "llx",
+}
+RUL = "LLX"
+
+ENX_MPO = 1 * 7
 enx = env.Resource("enx", capacity=ENX_MPO, anonymous=False)
 
 HUB = [EVSE(mpo=7) for _ in range(3)]
 
-EV1 = EV(dur=40, dsc=294.4, mpi=7)
-EV2 = EV(dur=50, dsc=294.4, mpi=7)
-EV3 = EV(dur=70, dsc=294.4, mpi=7)
+EV1 = EV(dur=20, dsc=140, mpi=7)
+EV2 = EV(dur=50, dsc=280, mpi=7)
+EV3 = EV(dur=70, dsc=290, mpi=7)
 
 TGC(name="TGC")
 
@@ -155,6 +186,8 @@ EVS = []  # list of all EVs after simulation
 waitingline = Queue("waitingline")
 env.run()
 
+# --------------------------------------------------------------------------
+# Reporting Results
 # --------------------------------------------------------------------------
 for ev in EVS:
     print(f"\n{ev.name()}")
@@ -165,14 +198,16 @@ for ev in EVS:
     #     f"{ev.name()} - dsc: {ev.dsc} - csc: {ev.csc} - toa: {ev.toa} - tod: {ev.tod}"
     # )
 
-for evse in HUB:
-    print(f"\n{evse.name()}")
-    evse.mon_kwh.print_statistics()
-    print(f"SE kWh: {evse.mon_kwh.duration(ex0=False) * evse.mon_kwh.mean(ex0=False)}")
-    # print(f"test: {evse.mon_kwh.duration_zero()}")
-    # print(f"tot: {tot}")
+# for evse in HUB:
+#     print(f"\n{evse.name()}")
+#     evse.mon_kwh.print_statistics()
+#     print(f"SE kWh: {evse.mon_kwh.duration(ex0=False) * evse.mon_kwh.mean(ex0=False)}")
+# print(f"test: {evse.mon_kwh.duration_zero()}")
+# print(f"tot: {tot}")
 
-    # print(evse.mon_kwh.as_dataframe())
-    # print(
-    #     f"{evse.name()} - pwr: {evse.pwr} "
-    # )
+# print(evse.mon_kwh.as_dataframe())
+# print(
+#     f"{evse.name()} - pwr: {evse.pwr} "
+# )
+
+# what if request is leass than time to charge TODO
